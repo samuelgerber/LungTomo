3da0c75a0295f275e419e51a22aaddfd53edbf16
Samuel Gerber <sgerber@uoregon.edu>
2017-08-17 10:27:05 -0400

    Fixing projection geometry

--- ForwardProject.cxx
+++ ForwardProject.cxx
@@ -10,6 +10,7 @@
 #include <rtkThreeDCircularProjectionGeometry.h>
 #include "rtkJosephForwardProjectionImageFilter.h"
 #include "itkImageFileWriter.h"
+#include "itkOrientImageFilter.h"
 
 int main(int argc, char **argv ){
 
@@ -71,6 +72,19 @@ int main(int argc, char **argv ){
   }
 
   InputImageType::Pointer ct = reader->GetOutput();
+  InputImageType::DirectionType direction = ct->GetDirection();
+  direction.SetIdentity();
+  ct->SetDirection(direction);
+  
+  /*
+  typedef itk::OrientImageFilter<InputImageType,InputImageType> OrientFilter;
+  OrientFilter::Pointer orienter = OrientFilter::New();
+  orienter->UseImageDirectionOn();
+  orienter->SetDesiredCoordinateOrientation(itk::SpatialOrientation::ITK_COORDINATE_ORIENTATION_LSA);//ITK_COORDINATE_ORIENTATION_RIP);
+  orienter->SetInput( ct );
+  orienter->Update();
+  ct = orienter->GetOutput();
+  */
 
   InputImageType::RegionType            ctRegion  = ct->GetLargestPossibleRegion();
   InputImageType::RegionType::SizeType  ctSize    = ctRegion.GetSize();
@@ -95,15 +109,22 @@ int main(int argc, char **argv ){
   InputImageType::Pointer projection = InputImageType::New();
   InputImageType::RegionType projectionRegion;
   InputImageType::RegionType::SizeType projectionSize = ctSize;
+  projectionSize[0] = ctSize[0];
+  projectionSize[1] = ctSize[1];
   projectionSize[2] = 1;
   projectionRegion.SetSize( projectionSize );
   InputImageType::RegionType::IndexType projectionIndex;
   projectionIndex.Fill(0);
   projectionRegion.SetIndex( projectionIndex );
-  InputImageType::PointType projectionOrigin;
-  projectionOrigin = ctOrigin;
+  InputImageType::PointType projectionOrigin = ctOrigin;
+  projectionOrigin.Fill(0);
+  //projectionOrigin[0] -= ctSize[0]*ctSpacing[0];
+  //projectionOrigin[1] = ctOrigin[2] - ctSize[2]*ctSpacing[2];
+  //projectionOrigin[2] -= ctSize[2]*ctSpacing[2];
   InputImageType::SpacingType projectionSpacing;
   projectionSpacing = ctSpacing;
+  projectionSpacing[0] = ctSpacing[0];
+  projectionSpacing[1] = ctSpacing[1];
   projectionSpacing[2] = 1;
 
   projection->SetOrigin( projectionOrigin );
@@ -121,18 +142,33 @@ int main(int argc, char **argv ){
   typedef rtk::ThreeDCircularProjectionGeometry GeometryType;
   GeometryType::Pointer geometry = GeometryType::New();
 
+  /*
   GeometryType::PointType sourcePosition = ctOrigin;
   sourcePosition[0] += ctSize[0] * ctSpacing[0] / 2;
-  sourcePosition[1] += ctSize[1] * ctSpacing[1] / 2;
-  GeometryType::PointType detectorPosition = sourcePosition;
-  sourcePosition[2] += ctSize[2] * ctSpacing[2];
+  sourcePosition[1] += ctSize[1] * 4*ctSpacing[1];
+  sourcePosition[2] += ctSize[2] * ctSpacing[2] / 2;
+  GeometryType::PointType detectorPosition = ctOrigin;
+  //detectorPosition[0] += ctSize[0] * ctSpacing[0] ;
+  //detectorPosition[1] += ctSize[1] * 4*ctSpacing[1];
+  //detectorPosition[2] += ctSize[2] * ctSpacing[2] ;
   GeometryType::VectorType detectorRow;
   detectorRow.Fill(0);
   detectorRow[0] = 1;
   GeometryType::VectorType detectorCol;
   detectorCol.Fill(0);
-  detectorCol[1] = 1;
-  geometry->AddProjection( sourcePosition, detectorPosition, detectorRow, detectorCol);
+  detectorCol[2] = -1;
+  */
+
+
+  geometry->AddProjection(
+        137 + ctSize[2]*ctSpacing[2] * 6, 
+        137 + ctSize[2]*ctSpacing[2] * 7, 
+        0, 
+        ctOrigin[0] + ctSize[0] * ctSpacing[0]/2 - projectionSpacing[0] * projectionSize[0]/2, 
+        ctOrigin[1] + ctSize[1] * ctSpacing[1]/2 - projectionSpacing[1] * projectionSize[1]/2, 
+        0, 0, 
+        ctOrigin[0] + ctSize[0]*ctSpacing[0]/2, 
+        ctOrigin[1] + ctSize[1]*ctSpacing[1]/2);
   std::cout << geometry << std::endl;
   forwardProjection->SetGeometry( geometry );
 
